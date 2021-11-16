e5c4db899517653099f8286827050d62352e5bdb
Filip Piwosz <39514318+Fasinir@users.noreply.github.com>
2021-11-16 18:22:08 +0100

    Criteria loader (#5)
    * loading criteria
    
    * fix
    
    * main corrections
    
    * removed hardcoded objects from main, code cleanup
    
    * removed unused annotations
    
    * unused imports

---
+++ src/main/java/com/agh/vacation/CriteriaJSONLoader.java
@@ -0,0 +1,62 @@
+package com.agh.vacation;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.RealMatrix;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+/**
+ * @author Filip Piwosz
+ */
+class CriteriaJSONLoader {
+    private static final String INDEX_MAP_FIELD_NAME = "indexMap";
+    private static final String MATRIX_FIELD_NAME = "matrix";
+
+    private CriteriaJSONLoader() {
+    }
+
+    static ComparisonMatrix<Criterion> loadCriteria(Path path) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode node = objectMapper.readTree(path.toFile());
+
+        JsonNode indexMapJsonNode = node.findValue(INDEX_MAP_FIELD_NAME);
+
+        IndexMap<Criterion> indexMap = new IndexMap<>(obtainIndexMap(indexMapJsonNode));
+
+        JsonNode matrixJsonNode = node.findValue(MATRIX_FIELD_NAME);
+        Fraction[][] fractions = objectMapper.treeToValue(matrixJsonNode, Fraction[][].class);
+        RealMatrix realMatrix = new Array2DRowRealMatrix(fromFractionArray(fractions));
+
+        return new ComparisonMatrix<>(realMatrix, indexMap);
+    }
+
+    private static double[][] fromFractionArray(Fraction[][] array) {
+        int length = array.length;
+        double[][] result = new double[length][length];
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                result[i][j] = array[i][j].value;
+            }
+        }
+        return result;
+    }
+
+    private static Map<Criterion, Integer> obtainIndexMap(JsonNode indexMapJsonNode) {
+        Map<Criterion, Integer> map = new HashMap<>();
+        Iterator<Entry<String, JsonNode>> iterator = indexMapJsonNode.fields();
+        while (iterator.hasNext()) {
+            Entry<String, JsonNode> entry = iterator.next();
+            Criterion criterion = new Criterion(entry.getKey());
+            Integer index = entry.getValue().intValue();
+            map.put(criterion, index);
+        }
+        return map;
+    }
+}
--- src/main/java/com/agh/vacation/Main.java
+++ src/main/java/com/agh/vacation/Main.java
@@ -1,18 +1,16 @@
 package com.agh.vacation;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.commons.math3.linear.Array2DRowRealMatrix;
-import org.apache.commons.math3.linear.RealMatrix;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static com.agh.vacation.CriteriaJSONLoader.loadCriteria;
 import static com.agh.vacation.CriteriaScoresCalculator.calculateCriteriaScores;
 import static com.agh.vacation.EigenvalueCalculator.calculateEigenvalues;
 import static com.agh.vacation.ResultCalculator.calculateResult;
@@ -20,60 +18,44 @@ import static com.agh.vacation.VacationDestinationComparisonMatricesCreator.crea
 
 public class Main {
     private static final String CITIES_PATH = "Cities";
-    //EXAMPLE CODE BELOW, THIS IS NOT FINAL
-    private static final Criterion vfm = new Criterion("Value for money");
-    private static final Criterion sights = new Criterion("Sights");
-    private static final Criterion museums = new Criterion("Museums");
-    private static final Criterion food = new Criterion("Food");
-    private static final Criterion nl = new Criterion("Night life");
+    private static final String CRITERIA_PATH = "criteria.json";
 
     public static void main(String[] args) {
+        //Load criteria and destinations
+        ComparisonMatrix<Criterion> criteriaComparisonMatrix;
+        List<VacationDestination> destinations;
 
-        //Load criteria
-        //TODO: load this from json and remove this example static object
-        ComparisonMatrix<Criterion> criteriaComparisonMatrix = criteriaComparisonMatrix();
+        try {
+            criteriaComparisonMatrix = loadCriteria(Path.of(CRITERIA_PATH));
+            destinations = loadDestinations();
+        } catch (IOException e) {
+            System.err.println("Couldn't load parameter(s)!");
+            System.err.println(e.getMessage());
+            return;
+        }
         //calculate priorities for criteria
         CriteriaPrioritiesMap criteriaPriorities =
                 new CriteriaPrioritiesMap(calculateEigenvalues(criteriaComparisonMatrix));
-        List<Criterion> criteria = new ArrayList<>(List.of(vfm, sights, museums, food, nl));
 
-        //Load destinations
-        List<VacationDestination> destinations = loadDestinations();
-        if (destinations.size() == 0) {
-            System.err.println("No cities !");
-            return;
-        }
+        List<Criterion> criteriaList = criteriaComparisonMatrix.
+                indexMap().
+                keySet().
+                stream().
+                toList();
+
         //Create a map of PC matrices based on destination ratings for each criterion
         ComparisonMatricesBasedOnCriteria comparisonMatricesBasedOnCriteria =
-                createComparisonMatricesBasedOnCriteria(criteria, destinations);
+                createComparisonMatricesBasedOnCriteria(criteriaList, destinations);
 
         //for each criterion calculate individual score of each destination
         //(score is not yet multiplied with proper criteria priority value)
         VacationCriteriaScoresMap criteriaScoresMap =
                 calculateCriteriaScores(destinations, comparisonMatricesBasedOnCriteria);
 
+        //calculate final result - sum(score * criterion priority)
         Result result = calculateResult(criteriaPriorities, criteriaScoresMap);
         System.out.println(result.display());
 
-    }
-
-
-    private static ComparisonMatrix<Criterion> criteriaComparisonMatrix() {
-        RealMatrix criteriaMatrix = new Array2DRowRealMatrix(new double[][]{
-                {1, 3, 5, 7, 9},
-                {1. / 3., 1, 3, 5, 7},
-                {1. / 5., 1. / 3., 1, 3, 5},
-                {1. / 7., 1. / 5., 1. / 3., 1, 3},
-                {1. / 9., 1. / 7., 1. / 5., 1. / 3., 1}
-        });
-        IndexMap<Criterion> indexMap = new IndexMap<>(new HashMap<>());
-        indexMap.put(vfm, 0);
-        indexMap.put(sights, 1);
-        indexMap.put(museums, 2);
-        indexMap.put(food, 3);
-        indexMap.put(nl, 4);
-
-        return new ComparisonMatrix<>(criteriaMatrix, indexMap);
     }
 
     static List<VacationDestination> loadDestinations() {
