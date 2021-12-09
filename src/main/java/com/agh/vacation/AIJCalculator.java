f7ceac72d6e96bd2365f470e660bd245c98cf4e9
Daniel Rosa <daniel_rosa@epam.com>
2021-12-09 23:27:48 +0100

    Add calculating from multiple experts using AIJ method

---
+++ src/main/java/com/agh/vacation/AIJCalculator.java
@@ -0,0 +1,48 @@
+package com.agh.vacation;
+
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.RealMatrix;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class AIJCalculator {
+    private AIJCalculator() {
+    }
+
+    /**
+     * We just return geometric mean of matrices
+     *
+     * @param criteria       list of criteria for loop
+     * @param expertMatrices multiple matrices to be aggregated
+     * @return single matrix calculated from geometric mean
+     */
+    static ComparisonMatricesBasedOnCriteria calculate(List<Criterion> criteria, List<ComparisonMatricesBasedOnCriteria> expertMatrices) {
+        if (expertMatrices.size() == 1)
+            return expertMatrices.get(0);
+
+        Map<Criterion, ComparisonMatrix<VacationDestination>> map = new HashMap<>();
+        var indexMap = expertMatrices.get(0).stream().findAny().get().getValue().indexMap();
+        var matrixSize = indexMap.keySet().size();
+        for (Criterion criterion : criteria) {
+            RealMatrix resultMatrix = new Array2DRowRealMatrix(matrixSize, matrixSize);
+            //OH GOD, WHY (╯°□°)╯︵ ┻━┻
+            List<RealMatrix> matrices = expertMatrices.stream().map(s -> s.stream().filter(x -> x.getKey().equals(criterion)).
+                    map(r -> r.getValue().matrix()).findAny().get()).toList();
+            for (int i = 0; i < matrixSize; i++) {
+                for (int j = 0; j < matrixSize; j++) {
+                    double newValue = 1;
+                    for (RealMatrix oneOfMatrices : matrices) {
+                        newValue *= oneOfMatrices.getEntry(i, j);
+                    }
+                    newValue = Math.pow(newValue, 1.0 / matrices.size());
+                    resultMatrix.setEntry(i, j, newValue);
+                }
+            }
+            ComparisonMatrix<VacationDestination> comparisonMatrix = new ComparisonMatrix<>(resultMatrix, indexMap);
+            map.put(criterion, comparisonMatrix);
+        }
+        return new ComparisonMatricesBasedOnCriteria(map);
+    }
+}
---
+++ src/main/java/com/agh/vacation/DestinationLoader.java
@@ -0,0 +1,50 @@
+package com.agh.vacation;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Stream;
+
+class DestinationLoader {
+    private static final String CITIES_PATH = "Cities\\";
+
+    private DestinationLoader() {
+
+    }
+
+    static List<VacationDestination> loadDestinations(String expertDirectoryName) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<VacationDestination> destinations = new ArrayList<>();
+        try (Stream<Path> pathStream = Files.list(Paths.get(CITIES_PATH + expertDirectoryName))) {
+            pathStream.map(Path::toFile)
+                    .forEach(path -> {
+                        try {
+                            destinations.add(objectMapper.readValue(path, VacationDestination.class));
+                        } catch (IOException e) {
+                            System.err.println("There was a problem with: " + path);
+                        }
+                    });
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        return destinations;
+    }
+
+    static List<List<VacationDestination>> loadMultipleExpertsDestinationRatings() throws IOException {
+        List<List<VacationDestination>> expertRatings = new LinkedList<>();
+        try (Stream<Path> pathStream = Files.list(Paths.get(CITIES_PATH))) {
+            pathStream.filter(Files::isDirectory).forEach(file ->
+                    expertRatings.add(loadDestinations(file.getFileName().toString()))
+            );
+        }
+        if(expertRatings.isEmpty())
+            throw new IOException("There are no expert files !");
+        return expertRatings;
+    }
+}
